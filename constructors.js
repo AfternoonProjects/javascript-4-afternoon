cfdd39db04ea5a931754ee15a694207511f3e6d0
Your Name <wamontgomery0068@gmail.com>
2018-12-20 14:20:01 -0600

    Completed. Use this as a reference.

--- constructors.js
+++ constructors.js
@@ -15,6 +15,13 @@
 
 // Code here
 
+function CarFactory(make, model){
+  return {
+    make,
+    model
+  };
+}
+
 
 
 ////////// PROBLEM 2 //////////
@@ -33,6 +40,8 @@ function Employee(name, email, hireDate) {
 */
 
 // Code here
+
+let bob = new Employee("Bob", "bob@gmail.com", "01-02-98");
 
 
 
@@ -55,4 +64,12 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
 
 // Code here
 
-
+function Car(make, model, year) {
+  (this.make = make),
+    (this.model = model),
+    (this.year = year),
+    (this.move = 0),
+    (this.moveCar = function() {
+      return (this.move += 10);
+    });
+}
